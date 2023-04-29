39d92bf7e693954c366a18f5a0e0380d2c489678
kimcno3 <kimcno3@gmail.com>
2023-04-29 16:06:48 +0900

    commit

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230429.145204';
+const cacheName = 'chirpy-20230429.150357';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
